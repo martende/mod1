a27a66d89ac39ed06334bb6f3d0f06ab67616fc2
Aleh Biruk <aleh.biruk@ltnglobal.com>
2020-10-16 11:38:39 +0200

    de

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/martende/mod1
 
 go 1.15
 
-require github.com/martende/mod3 v1.1.1
+require (
+	github.com/golang/protobuf v1.4.0
+	github.com/martende/mod3 v1.1.1
+)
--- mod.go
+++ mod.go
@@ -1,6 +1,9 @@
 package mod1
 
 import "github.com/martende/mod3"
+import "github.com/golang/protobuf/proto"
+
+var _ = proto.Marshal
 
 func HelloWorld() int {
 	return 10 + mod3.HelloWorld()
